0edd5d93b3527306e79e4686b30da9943a6303f7
root <3294005475a@gmail.com>
2019-03-31 19:35:45 +0800

    updatge

---
+++ boundline.cpp
@@ -0,0 +1,6 @@
+#include "boundline.h"
+
+BoundLine::BoundLine()
+{
+
+}
---
+++ boundline.h
@@ -0,0 +1,11 @@
+#ifndef BOUNDLINE_H
+#define BOUNDLINE_H
+
+
+class BoundLine
+{
+public:
+    BoundLine();
+};
+
+#endif // BOUNDLINE_H
