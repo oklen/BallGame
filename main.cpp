f63cf06ba0611564a849de64385bd403cc7e6a9b
root <3294005475a@gmail.com>
2019-04-01 12:14:37 +0800

    update

--- main.cpp
+++ main.cpp
@@ -7,10 +7,10 @@
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-    GlobalControl globalContorl;
+//    GlobalControl globalContorl;
 
-//    ControlButton cb;
-//    cb.show();
+    ControlButton cb;
+    cb.show();
 //    gmr.mainWindow.showFullScreen();
 //    w.show();
 
